a26cf9f8cfc7f65a275c5e81d4b7eb26ad8ac615
siddharthaupreti <sid876upreti@gmail.com>
2025-09-16 18:35:02 +0545

    Initial commit - Producer Sid Website

---
+++ script.js
@@ -0,0 +1,8 @@
+//  JavaScript for Mobile Menu
+        const mobileMenuButton = document.getElementById('mobile-menu-button');
+        const mobileMenu = document.getElementById('mobile-menu');
+
+        mobileMenuButton.addEventListener('click', () => {
+            mobileMenu.classList.toggle('hidden');
+        });
+    
